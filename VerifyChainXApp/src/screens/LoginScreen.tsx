e7315cb7f27d3573d70468013d91bfb64a96ce66
0xvikasrushi <0xvikas@gmail.com>
2023-12-09 01:45:14 +0530

    .env setup and navigation fixes

--- VerifyChainXApp/src/screens/LoginScreen.tsx
+++ VerifyChainXApp/src/screens/LoginScreen.tsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { AppStackScreenProps } from "../types";
+import { AUTH_URL } from "../shared/constant";
 
-type LoginScreen = AppStackScreenProps<"LoginScreen">;
-const ProfileScreen: React.FC<LoginScreen> = () => {
+type LoginScreenProps = AppStackScreenProps<"LoginScreen">;
+const LoginScreen: React.FC<LoginScreenProps> = () => {
+  const a = AUTH_URL;
+  console.log(a);
   return (
     <View>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
+      <Text>ProfileScreen</Text>
       <Text>ProfileScreen</Text>
     </View>
   );
 };
 
-export default ProfileScreen;
+export default LoginScreen;
--- VerifyChainXApp/src/shared/constant.ts
+++ VerifyChainXApp/src/shared/constant.ts
@@ -1,1 +1,4 @@
+import Config from "react-native-config";
 export const headerOptions = { headerShown: false };
+
+export const AUTH_URL = Config.AUTH_URL;
