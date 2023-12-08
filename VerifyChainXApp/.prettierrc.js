de459731536d9d0227fb2f7de437e92726867c65
0xvikasrushi <0xvikas@gmail.com>
2023-12-08 23:25:29 +0530

    fix: lint and eslint

--- VerifyChainXApp/.prettierrc.js
+++ VerifyChainXApp/.prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  singleQuote: false,
+  semi: true,
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
 };
