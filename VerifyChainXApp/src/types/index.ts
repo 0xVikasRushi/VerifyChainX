6ecf275832b1b59b92afcb109a2854164bdc5be4
tajbaba999 <tajbaba153@gmail.com>
2023-12-10 01:51:05 +0530

    feat: Event

--- VerifyChainXApp/src/types/index.ts
+++ VerifyChainXApp/src/types/index.ts
@@ -21,3 +21,22 @@ export interface CardsProps {
   onPress?: () => void;
   bgColor?: string;
 }
+
+export type Event = {
+  _id: string;
+  event_title: string;
+  event_description: string;
+  event_owner: string;
+  attestation_uid: string;
+  num_voters: number;
+  credits_per_voter: number;
+  start_event_date: string;
+  end_event_date: string;
+  event_data: string;
+  id: string;
+
+  secret_key: string;
+  created_at: string;
+  __v: number;
+  timeLeft?: number;
+};
