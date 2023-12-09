561355a2349c1864e6b3e6a8baeec67fd0bae128
tajbaba999 <tajbaba153@gmail.com>
2023-12-10 01:43:12 +0530

    feat:ProfileScren onchain

--- VerifyChainXApp/src/screens/ProfileScreen.tsx
+++ VerifyChainXApp/src/screens/ProfileScreen.tsx
@@ -1,18 +1,36 @@
-import { Avatar, Image } from "native-base";
-import React from "react";
-import { SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Avatar, Image, Box } from "native-base";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ToastAndroid,
+} from "react-native";
 import { AppStackScreenProps } from "../types";
+import Clipboard from "@react-native-clipboard/clipboard";
 
 type ProfileScreenProps = AppStackScreenProps<"ProfileScreen">;
 const ProfileScreen: React.FC<ProfileScreenProps> = () => {
+  const [onchainVerified, setonchainVerified] = useState(true);
+  const [offchainVerified, setoffchainVerified] = useState(true);
+  const [copiedText, setCopiedText] = useState("");
+  const [addressText, setAdressText] = useState("0x742d35Cc6634C0532925a3b844Bc454e4438f44e");
   const isVerified = true;
+
+  const copyToClipboard = () => {
+    Clipboard.setString(addressText);
+    console.log(`Copied to Text : ${addressText}`);
+  };
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <ScrollView contentContainerStyle={styles.container}>
-        <View style={styles.header}>
+        <Box width={"100%"} alignItems={"center"} style={styles.header}>
           <Text style={styles.title}>Profile</Text>
-        </View>
+        </Box>
 
         <View style={styles.profile}>
           <Avatar
@@ -22,26 +40,70 @@ const ProfileScreen: React.FC<ProfileScreenProps> = () => {
             }}
           />
           <View style={styles.profileDetails}>
-            <Text style={styles.profileName}>Taj Baba</Text>
-            {isVerified && (
-              <Image
-                source={require("../public/verify.png")}
-                height={5}
-                width={5}
-                alt="Verification"
-              />
-            )}
+            <Text style={styles.profileName}>User</Text>
           </View>
+          <Box flexDirection={"row"}>
+            <Text style={styles.onchainText}>
+              {onchainVerified ? "onchainVerified-" : "notOnchainVerified-"}
+            </Text>
+            <Box marginTop={1}>
+              {onchainVerified ? (
+                <Image
+                  source={require("../public/verify.png")}
+                  height={5}
+                  width={5}
+                  alt="Verification"
+                />
+              ) : (
+                <Image
+                  source={require("../public/unverified.png")}
+                  height={5}
+                  width={5}
+                  alt="onChainverified"
+                />
+              )}
+            </Box>
+          </Box>
+          <Box flexDirection={"row"}>
+            <Text style={styles.offchainText}>
+              {offchainVerified ? "OffchainVerified-" : "notOffchainVerified-"}
+            </Text>
+            <Box marginTop={1}>
+              {offchainVerified ? (
+                <Image
+                  source={require("../public/verify.png")}
+                  height={5}
+                  width={5}
+                  alt="Verification"
+                />
+              ) : (
+                <Image
+                  source={require("../public/unverified.png")}
+                  height={5}
+                  width={5}
+                  alt="onChainverified"
+                />
+              )}
+            </Box>
+          </Box>
 
-          <Text style={styles.ethereumAddress}>{isVerified ? "Verified" : "Not Verified"}</Text>
-          <Text style={styles.ethereumAddress}>
-            {isVerified ? "0x4a3...8f2d" : "Not available"}
-          </Text>
-          <TouchableOpacity
-            onPress={() => {
-              console.log("Logout");
-            }}
-          >
+          <Box flexDirection={"row"}>
+            <Text style={styles.ethereumAddress}>
+              {isVerified ? `${addressText}` : "Not available"}
+            </Text>
+            <TouchableOpacity onPress={copyToClipboard}>
+              <Box flex={1} marginTop={1.5}>
+                <Image
+                  source={require("../public/clipboard.png")}
+                  height={5}
+                  width={5}
+                  alt="onChainverified"
+                />
+              </Box>
+            </TouchableOpacity>
+          </Box>
+
+          <TouchableOpacity onPress={() => console.log("Log out")}>
             <View style={styles.profileAction}>
               <Text style={styles.profileActionText}>Log out</Text>
             </View>
@@ -72,10 +134,10 @@ const styles = StyleSheet.create({
   },
   profile: {
     marginTop: 24,
-    padding: 16,
+    padding: 26,
     alignItems: "center",
     backgroundColor: "#fff",
-    borderWidth: 1,
+    borderWidth: 5,
     borderColor: "#e3e3e3",
     borderRadius: 12,
   },
@@ -85,15 +147,16 @@ const styles = StyleSheet.create({
     marginTop: 12,
   },
   profileName: {
-    fontSize: 20,
+    fontSize: 27,
     fontWeight: "600",
     color: "#090909",
     marginRight: 8,
   },
   ethereumAddress: {
-    marginTop: 6,
-    fontSize: 16,
-    fontWeight: "400",
+    marginTop: 10,
+    fontSize: 11,
+    overflow: "hidden",
+    fontWeight: "600",
     color: "#848484",
   },
   profileAction: {
@@ -110,6 +173,15 @@ const styles = StyleSheet.create({
     fontSize: 15,
     fontWeight: "600",
     color: "#fff",
+  },
+  onchainText: {
+    color: "#353935",
+    fontSize: 17,
+  },
+  offchainText: {
+    color: "#353935",
+    letterSpacing: 1,
+    fontSize: 15,
   },
 });
 
