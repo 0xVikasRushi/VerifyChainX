8a2411bdc53c4764fbf46f1a0e1e99ddd9a23b58
0xvikasrushi <0xvikas@gmail.com>
2023-12-10 06:31:56 +0530

    feat: dashboard card

--- VerifyChainXApp/src/types/index.ts
+++ VerifyChainXApp/src/types/index.ts
@@ -13,3 +13,11 @@ export type AppStackScreenProps<T extends keyof AppParamList> = NativeStackScree
   AppParamList,
   T
 >;
+
+export interface CardsProps {
+  title?: string;
+  iconname?: any;
+  fontsize?: string;
+  onPress?: () => void;
+  bgColor?: string;
+}
